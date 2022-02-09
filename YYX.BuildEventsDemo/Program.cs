fd27475224a961ee0841e27e8e2b540cb09ff2cd
Andy Yin <ayin@aptean.com>
2022-02-09 14:33:05 +0800

    Add project files.

---
+++ YYX.BuildEventsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+var rowText = "eWlueW9uZ3hpYW4=";
+byte[] fromBase64String = Convert.FromBase64String(rowText);
+var text = Encoding.UTF8.GetString(fromBase64String);
+
+Console.WriteLine(text);
+
+var bytes = Encoding.UTF8.GetBytes(text);
+var base64String = Convert.ToBase64String(bytes);
+
+//Console.ReadKey();
